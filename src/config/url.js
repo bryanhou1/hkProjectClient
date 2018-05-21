58fb75f86a8045e0f40484c74665a487d5e9eac1
Bryan Hou <bryan.hou94@gmail.com>
2018-05-21 13:04:25 +0800

    change backend url

--- src/config/url.js
+++ src/config/url.js
@@ -1,6 +1,6 @@
 
 // export const API_URL = 'http://147.8.134.246:3000/'
-// export const API_URL = 'https://smile.hku.hk/ARGs_OSP';
-export const API_URL = process.env.NODE_ENV === 'production' ?
-  'http://147.8.134.246:3000/' :
-  'http://localhost:3000';
+export const API_URL = 'https://smile.hku.hk/ARGs_OSP';
+// export const API_URL = process.env.NODE_ENV === 'production' ?
+//   'http://147.8.134.246:3000/' :
+//   'http://localhost:3000';
