ec1cbedf421991ef2b52b5902a700e58421fd201
Bryan Hou <bryan.hou94@gmail.com>
2018-06-03 16:58:05 +0800

    remove conflicting props for images

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -22,11 +22,11 @@ class Carousel extends React.Component {
       <Slider {...settings}>
         <Segment padded>
           <Label attached='bottom'>Fig. 1. The phylogenetic relationship of all 54,718 bacterial genomes of 45 phyla in WGD and the occurrence of ARGs (blue nodes) and Rank I ARGs (red nodes). The pathogenic strains were indicated by purple edges.</Label>
-          <Image size="massive" src={Fig1} rounded fluid centered/>
+          <Image size="massive" src={Fig1} rounded centered/>
         </Segment>
         <Segment padded>
           <Label attached='bottom'>Fig. 2. The global map of metagenomic datasets in MGD. The size of the datasets (nodes) was proportional to the number of samples and the color was differentiated by the eco-type.</Label>
-          <Image size="massive" src={Fig2} rounded fluid centered/>
+          <Image size="massive" src={Fig2} rounded centered/>
         </Segment>
       </Slider>
     );
